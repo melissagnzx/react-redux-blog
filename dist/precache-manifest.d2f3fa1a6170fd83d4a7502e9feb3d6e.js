1ad319293a27f47f9e588131e534a8c0246011fa
gonzalm7 <gonzalm7@wwu.edu>
2019-10-25 14:07:06 -0700

    finished basic functions

---
+++ dist/precache-manifest.d2f3fa1a6170fd83d4a7502e9feb3d6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9617617f30a9caa98f6b0116b69b9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bbcea0e31a8a0a3dd4f",
+    "url": "/static/css/main.37c2a795.chunk.css"
+  },
+  {
+    "revision": "a64a02a3d6d84a1d98e7",
+    "url": "/static/js/2.6fa00775.chunk.js"
+  },
+  {
+    "revision": "7bbcea0e31a8a0a3dd4f",
+    "url": "/static/js/main.434393b4.chunk.js"
+  },
+  {
+    "revision": "e5873e7c92a4a744f82c",
+    "url": "/static/js/runtime-main.1c69ef4d.js"
+  }
+]);
